85a93e16bf730ad9188514858672830754822ead
Toxic <localgod13@gmail.com>
2025-05-10 01:31:35 -0700

    Update threeRenderer.js

--- threeRenderer.js
+++ threeRenderer.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.176.0/build/three.module.js';
 
 /**
  * Three.js Renderer for game effects
